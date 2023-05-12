cacadcc45db555672dcad3d9f8aa6da490e7cc0a
Lucas Mario <lucas.brightsoft@gmail.com>
2023-05-12 17:32:59 +0700

    fix: show data home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,9 +16,8 @@ class HomeController extends Controller
     public function home_page()
     {
         $list_categories = BlogCategory::Where(['status' => 1, 'is_home_page' => 1])->select('id', 'category_name', 'slug')->get();
-        $list_posts = Blog::orderBy('published_date')->take(3)->select('id','slug', 'banner', 'title', 'published_date', 'short_description')->get();
         $slider_banner = SliderBanner::first();
 
-        return view('frontend.home_page', compact('list_categories', 'slider_banner', 'list_posts'));
+        return view('frontend.home_page', compact('list_categories', 'slider_banner'));
     }
 }
