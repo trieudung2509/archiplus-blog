eae13ef8fc3b06d0df065802f7eef56c2bb1be6f
Lucas Mario <lucas.brightsoft@gmail.com>
2023-05-16 00:23:25 +0700

    fix: bug css

--- resources/views/frontend/home_page.blade.php
+++ resources/views/frontend/home_page.blade.php
@@ -109,67 +109,64 @@
                 </div>
             </div>
         </div>
-        <div class="vc_row wpb_row vc_row-fluid vc_custom_1673232508727 section--post">
+        <div class="vc_row wpb_row vc_row-fluid section--post">
             <div class="wpb_column vc_column_container vc_col-sm-12 vc_col-has-fill">
                 <div class="vc_column-inner vc_custom_1673232448314">
                 <div class="wpb_wrapper">
                     <div class="edgtf-link-section-holder edgtf-appear-fx edgtf-appear">
-                    <div class="edgtf-link-section-title-holder">
-                        <h2 class="edgtf-link-section-title"> DỰ ÁN </h2>
-                    </div>
-                    <?php 
-                        $categories_menu = \App\BlogCategory::where(['status' => 1, 'is_show_menu' =>  1])->get();
-                    ?>
-                        @foreach( $categories_menu as $cate)
-                        <div class="edgtf-single-link-section-holder">
-                            <a href="{{ route('news_page', ['slug' => $cate->slug]) }}" class="edgtf-link-section">
-                            <div class="edgtf-single-link-title-holder">
-                                <h5 class="edgtf-single-link-title"> {{ $cate->category_name }} </h5>
-                            </div>
-                            </a>
+                        <div class="edgtf-link-section-title-holder">
+                            <h2 class="edgtf-link-section-title"> DỰ ÁN </h2>
                         </div>
                         <?php 
-                            $item_posts = \App\Blog::where(['status' => 1, 'category_id' => $cate->id])->orderBy('published_date')
-                                ->take(3)->select('id','slug', 'banner', 'title', 'published_date', 'short_description')->get();
+                            $categories_menu = \App\BlogCategory::where(['status' => 1, 'is_show_menu' =>  1])->get();
                         ?>
-                        <div class="row" style="margin: 15px 0px 0px;">
-                            @foreach($item_posts as $post)
-                                <article class="col-lg-4 col-md-6">
-                                    <a href="{{ route('detail_page', ['slug' => $post->slug]) }}" class="tile tile--full-height">
-                                    <div class="tile__img">
-                                        <div class="tile__img-box tile__img-box--height-sm animation-imageScale lazy" id="news_{{ $post->id }}" data-name="news_{{ $post->id }}" data-style="    @media screen and (max-width:500px) {  #news_{{ $post->id }} {  background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner);  }}?w=600&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:501px) and (max-width:768px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=700&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:769px) and (max-width:991px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=500&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:992px) and (max-width:1199px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=500&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:1200px) and (max-width:1400px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=600&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:1401px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=600&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }   ">
-                                            <picture>
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=499&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 499px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=500&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 500px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=641&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 641px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=769&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 769px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=902&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 902px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1025&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1025px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1200&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1200px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1582&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1582px)">
-                                            <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1920&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1920px)">
-                                            <img class=" lazy" data-src="{{ uploaded_asset($post->banner) }}?w=3860&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" alt="{{ $post->title }}">
-                                            </picture>
+                        @foreach( $categories_menu as $cate)
+                            <div class="edgtf-single-link-section-holder">
+                                <a href="{{ route('news_page', ['slug' => $cate->slug]) }}" class="edgtf-link-section">
+                                <div class="edgtf-single-link-title-holder">
+                                    <h5 class="edgtf-single-link-title"> {{ $cate->category_name }} </h5>
+                                </div>
+                                </a>
+                            </div>
+                            <?php 
+                                $item_posts = \App\Blog::where(['status' => 1, 'category_id' => $cate->id])->orderBy('published_date')
+                                    ->take(3)->select('id','slug', 'banner', 'title', 'published_date', 'short_description')->get();
+                            ?>
+                            <div class="row" style="margin: 15px 0px 0px;">
+                                @foreach($item_posts as $post)
+                                    <article class="col-lg-4 col-md-6">
+                                        <a href="{{ route('detail_page', ['slug' => $post->slug]) }}" class="tile tile--full-height">
+                                        <div class="tile__img">
+                                            <div class="tile__img-box tile__img-box--height-sm animation-imageScale lazy" id="news_{{ $post->id }}" data-name="news_{{ $post->id }}" data-style="    @media screen and (max-width:500px) {  #news_{{ $post->id }} {  background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner);  }}?w=600&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:501px) and (max-width:768px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=700&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:769px) and (max-width:991px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=500&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:992px) and (max-width:1199px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=500&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:1200px) and (max-width:1400px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=600&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }    @media screen and (min-width:1401px) {  #news_{{ $post->id }} { background-position: 100% 100%; background-image: url('{{ uploaded_asset($post->banner) }}?w=600&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges')  } }   ">
+                                                <picture>
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=499&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 499px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=500&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 500px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=641&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 641px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=769&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 769px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=902&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 902px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1025&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1025px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1200&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1200px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1582&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1582px)">
+                                                <source data-srcset="{{ uploaded_asset($post->banner) }}?w=1920&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" media="(max-width: 1920px)">
+                                                <img class=" lazy" data-src="{{ uploaded_asset($post->banner) }}?w=3860&h=&fit=crop&q=80&auto=format&fm=png&crop=faces,edges" alt="{{ $post->title }}">
+                                                </picture>
+                                            </div>
+                                            <span class="tile__img-curtain slideInUp"></span>
                                         </div>
-                                        <span class="tile__img-curtain slideInUp"></span>
-                                    </div>
-                                    <div class="tile__content content">
-                                        <h2 class="headline-5">{{ $post->title }}</h2>
-                                        <p class="content__small-text">13 pics</p>
-                                        <svg version="1.1" id="btn-arrow-1949404842" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="18px" height="4px" viewBox="0 0 18 4" enable-background="new 0 0 18 4" xml:space="preserve">
-                                        <polyline fill="none" stroke="#fff" stroke-miterlimit="10" points="0,3.508 16.809,3.508 13.686,0.342 "></polyline>
-                                        </svg>
-                                    </div>
-                                    </a>
-                                </article>
-                            @endforeach
-                        </div>
+                                        <div class="tile__content content">
+                                            <h2 class="headline-5">{{ $post->title }}</h2>
+                                            <p class="content__small-text">13 pics</p>
+                                            <svg version="1.1" id="btn-arrow-1949404842" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="18px" height="4px" viewBox="0 0 18 4" enable-background="new 0 0 18 4" xml:space="preserve">
+                                            <polyline fill="none" stroke="#fff" stroke-miterlimit="10" points="0,3.508 16.809,3.508 13.686,0.342 "></polyline>
+                                            </svg>
+                                        </div>
+                                        </a>
+                                    </article>
+                                @endforeach
+                            </div>
                         @endforeach
-                       
-                      
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
         </div>
